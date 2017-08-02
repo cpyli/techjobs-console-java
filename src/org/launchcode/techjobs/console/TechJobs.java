68bb8b29324db501f864c16b478f8851a0824c20
cpyli <cpyli@uw.edu>
2017-08-01 22:47:55 -0700

    Updated assignment

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -11,7 +12,7 @@ public class TechJobs {
 
     private static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -61,7 +62,9 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    // Entered new search method.
+                    printJobs(JobData.findByValue(searchField, searchTerm));
+
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -103,7 +106,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -111,6 +114,24 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+
+            System.out.println("\nNo job matches found. Please search again.");
+
+        } else {
+
+            for (HashMap<String, String> job : someJobs) {
+
+                System.out.println("\n*****");
+
+                for (Map.Entry<String, String> j : job.entrySet()) {
+                    System.out.println(j.getKey() + ": " + j.getValue());
+
+                }
+
+                System.out.println("*****");
+
+            }
+        }
     }
 }
